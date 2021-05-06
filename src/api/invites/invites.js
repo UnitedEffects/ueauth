decf0aac5525bc1c4fd3cdaada70c9750bec0884
Bo Motlagh <borzou@theboeffect.com>
2021-05-06 16:36:51 -0400

    validation of resources being invited to

--- src/api/invites/invites.js
+++ src/api/invites/invites.js
@@ -1,18 +1,38 @@
 import dal from './dal';
 import helper from '../../helper';
+import group from '../authGroup/group';
+import Boom from '@hapi/boom';
 //import plugin from '../../notifications';
 //const config = require('../../config');
 
-export default {
+const inv = {
 	async createInvite(userId, data, authGroup) {
 		const invite = JSON.parse(JSON.stringify(data));
 		invite.authGroup = authGroup.id;
 		const days = invite.daysToExpire || 7;
 		invite.expiresAt = new Date().setDate(new Date().getDate() + days);
-		// todo validate resource Ids with a function here...
+		const valEr = [];
+		await Promise.all(invite.resources.map(async (resource) => {
+			try {
+				if(resource) await inv.validateResourceIds(resource.resourceType, resource.resourceId);
+			} catch (error) {
+				valEr.push(error);
+			}
+		}))
+		if(valEr.length!==0) throw Boom.badRequest(valEr.join('; '));
 		const invResult = await dal.createInvite(invite);
 		// todo notification
 		return invResult;
+	},
+	async validateResourceIds(r, id) {
+		switch (r) {
+			case 'group':
+				const ag = await group.getOne(id);
+				if(!ag) throw `Resource id ${id} does not exist`;
+				return;
+			default:
+				throw `Resource type ${r} does not exist or has not been implemented yet`;
+		}
 	},
 
 	async getInvites(authGroupId, q) {
@@ -32,3 +52,5 @@ export default {
 		return dal.inviteAuthorizedLookup(authGroupId, sub, type);
 	}
 };
+
+export default inv;
