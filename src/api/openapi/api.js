2c84383465ee5d73147e9d03df30557e3a336ba6
Bo Motlagh <borzou@theboeffect.com>
2021-08-25 15:24:50 -0400

    updating messaging and tested requests work

--- src/api/openapi/api.js
+++ src/api/openapi/api.js
@@ -8,14 +8,17 @@ export default {
     },
     async serveApiJson(req, res, next) {
         try{
-            const swag = JSON.parse(JSON.stringify(swagger));
+            let swag = JSON.parse(JSON.stringify(swagger));
             swag.info.version = pJson.version;
             swag.info.title = pJson.name;
             swag.info['x-logo'].url = pJson.logo;
-            swag.info.description = (!req.params.group) ? swag.info.description : `<h3>AuthGroup and OIDC security set to: ${req.authGroup.name}</h3>${swag.info.description}`;
+            swag.info.description = (!req.params.group) ? swag.info.description : `<h3>AuthGroup and OIDC security set to: ${req.authGroup.name}</h3><p><i>Please note, you will still need to enter a value into the required group fields to make openapi requests; however, for your convenience, the ID of the authgroup you've selected, <strong>${req.authGroup.name} - ${req.authGroup.id}</strong>, is displayed and used rather than whatever you may enter in the field.</i></p>${swag.info.description}`;
             if (config.SWAGGER) swag.servers = [{url: `${config.PROTOCOL}://${config.SWAGGER}`}];
             if(req.params.group) {
                 swag.components.securitySchemes.openId.openIdConnectUrl = `${config.PROTOCOL}://${config.SWAGGER}/${req.authGroup.id}/.well-known/openid-configuration`
+                let temp = JSON.stringify(swag);
+                temp = temp.replace(/{group}/g, req.params.group);
+                swag = JSON.parse(temp);
             }
             return res.json(swag);
         }catch (error) {
