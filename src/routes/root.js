ba4764c55dbdeac5df5a4afa72c31734f61a5b88
Bo Motlagh <borzou@theboeffect.com>
2021-08-25 16:07:54 -0400

    fixing reference error

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import openapi from '../../src/api/openapi/api';
+import openapi from '../api/openapi/api';
 import m from '../middleware';
 
 const router = express.Router();
