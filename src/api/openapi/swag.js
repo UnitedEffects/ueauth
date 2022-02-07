abd3564444a22287dee0f5f75501cc14beb90937
Bo Motlagh <borzou@theboeffect.com>
2022-02-07 17:36:37 -0500

    Fixes #132

--- src/api/openapi/swag.js
+++ src/api/openapi/swag.js
@@ -5,7 +5,11 @@ export default {
 	updateSwag(swag, authGroup, params, cd = undefined) {
 		swag.info.version = pJson.version;
 		swag.info.title = pJson.name;
-		swag.info['x-logo'].url = pJson.logo;
+		swag.info['x-logo'].url = (authGroup?.config?.ui?.skin?.logo) ?
+			authGroup?.config?.ui?.skin?.logo :
+			(authGroup?.config?.ui?.skin?.splashImage) ?
+				authGroup?.config?.ui?.skin?.splashImage :
+				pJson.logo;
 		swag.info.description = (config.ENV !== 'production') ? `${swag.info.description}<h4 style='color:red'>WARNING: THIS IS A DEMO AND TEST ENVIRONMENT. ALL DATA IS EPHEMERAL AND SUBJECT TO DELETION.</h4>` : swag.info.description;
 		swag.info.description = (!params.group) ? swag.info.description : `<h3>AuthGroup and OIDC security set to: ${authGroup.name}</h3><p><i>Please note, you will still need to enter a value into the required group fields to make openapi requests; however, for your convenience, the ID of the authgroup you've selected, <strong>${authGroup.name} - ${authGroup.id}</strong>, is displayed and used rather than whatever you may enter in the field.</i></p>${swag.info.description}`;
 		if (config.SWAGGER) swag.servers = [{url: `${config.PROTOCOL}://${cd || config.SWAGGER}`}];
