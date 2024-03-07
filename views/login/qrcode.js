266c22094451b41c1d35b023d0f8a87cb2007562
Bo Motlagh <bo@unitedeffects.com>
2024-03-07 17:03:21 -0500

    cleanup - issue seems resolved. Final testing in QA

--- views/login/qrcode.js
+++ views/login/qrcode.js
@@ -2,7 +2,6 @@ window.addEventListener( 'load', async function () {
     (function() {
         $('#main').css({ 'padding-top': '65px'});
         if(qrCode) {
-            console.info('QR CODE', qrCode);
             var qr = new QRious({
                 element: document.getElementById('qrcode'),
                 size: 500,
